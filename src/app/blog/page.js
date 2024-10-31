6a1e55991261f4989116eda95ad168436d6934d6
Dmytro Zuiev <zuyevdmytro@gmail.com>
2024-10-31 15:19:50 +0000

    Updated production domain fetching

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,20 +1,25 @@
 // 'use client'
 
+import getDomain from '@/app/lib/getDomain';
+
 async function getData() {
   // 1. API endpoints
-  // const endpoint = 'http://localhost:3000/api/posts';
-  // const res = await fetch(endpoint);
+  const domain = getDomain();
+  const endpoint = `${domain}/api/posts`;
+  const res = await fetch(endpoint);
 
-  // if (!res.ok) throw new Error('Failed to fetch data!');
+  if (!res.ok) throw new Error('Failed to fetch data!');
 
-  // const data = await res.json();
-  // return data;
-  return { items: [] };
+  const data = await res.json();
+  return data;
+  // return { items: [] };
 }
 
 export default async function BlogPage() {
   const data = await getData();
   const items = data && data.items ? [...data.items] : [];
+  console.log(items);
+  console.log(process.env.PUBLIC_DOMAIN);
   return (
     <main>
       <h1>Hello World</h1>
