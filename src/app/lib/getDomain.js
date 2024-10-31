00dec417dce5c76bb737f7e927578087a514097c
Dmytro Zuiev <zuyevdmytro@gmail.com>
2024-10-31 16:25:42 +0000

    Updated getDomain

--- src/app/lib/getDomain.js
+++ src/app/lib/getDomain.js
@@ -1,4 +1,6 @@
 export default function getDomain() {
+  console.log('NEXT_PUBLIC_VERCEL_ENV:', process.env.NEXT_PUBLIC_VERCEL_ENV);
+  console.log('NEXT_PUBLIC_VERCEL_URL:', process.env.NEXT_PUBLIC_VERCEL_URL);
   const protocol =
     process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? 'https' : 'http';
   const domain = process.env.NEXT_PUBLIC_VERCEL_URL
