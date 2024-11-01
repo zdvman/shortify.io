f49b10b37838da8e71963d31d01720d1ca8bb972
Dmytro Zuiev <zuyevdmytro@gmail.com>
2024-11-01 13:12:29 +0000

    Updated endpoint to relative api

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -14,7 +14,7 @@ import getDomain from '@/app/lib/getDomain';
 async function getData() {
   // 1 endpoint - API?
   const domain = getDomain();
-  const endpoint = `${domain}/api/posts`; // -> third party api request??
+  const endpoint = `/api/posts`; // -> third party api request??
   // const res = await fetch(endpoint, { next: { revalidate: 10 } }); // HTTP GET
   const res = await fetch(endpoint, { cache: 'no-store' }); // HTTP GET
 
