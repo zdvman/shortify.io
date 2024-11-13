aa74429b2a8e028530902dd37fa5d5ea2241f329
zdvman <zuyevdmytro@gmail.com>
2024-11-13 11:40:54 +0000

    relations were added in schema.js

--- src/app/lib/schema.js
+++ src/app/lib/schema.js
@@ -8,17 +8,18 @@ import {
   serial,
   varchar,
   integer,
-} from 'drizzle-orm/pg-core';
+  relations,
+} from "drizzle-orm/pg-core";
 
 export const linksTable = pgTable(
-  'links',
+  "links",
   {
-    id: serial('id').primaryKey().notNull(),
-    url: text('url').notNull(),
-    short: varchar('short', { length: 50 }),
-    createdAt: timestamp('created_at').defaultNow(),
+    id: serial("id").primaryKey().notNull(),
+    url: text("url").notNull(),
+    short: varchar("short", { length: 50 }),
+    createdAt: timestamp("created_at").defaultNow(),
   },
-  (links) => [uniqueIndex('url_idx').on(links.url)]
+  (links) => [uniqueIndex("url_idx").on(links.url)]
 );
 
 // links --> link --> has as many visits as it has been visited
@@ -26,12 +27,12 @@ export const linksTableRelations = relations(linksTable, ({ many }) => ({
   visits: many(visitsTable),
 }));
 
-export const visitsTable = pgTable('visits', {
-  id: serial('id').primaryKey().notNull(),
-  linkId: integer('link_id')
+export const visitsTable = pgTable("visits", {
+  id: serial("id").primaryKey().notNull(),
+  linkId: integer("link_id")
     .notNull()
     .references(() => linksTable.id),
-  createdAt: timestamp('created_at').defaultNow(),
+  createdAt: timestamp("created_at").defaultNow(),
 });
 
 // visits --> visit --> one link
