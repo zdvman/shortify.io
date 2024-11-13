48a3d08c23a22d0dd4534f393c0462c99a5e5a7d
Dmytro Zuiev <zuyevdmytro@gmail.com>
2024-11-13 09:07:45 +0000

    new table visitors added

--- src/app/lib/schema.js
+++ src/app/lib/schema.js
@@ -7,15 +7,37 @@ import {
   pgTable,
   serial,
   varchar,
-} from "drizzle-orm/pg-core";
+  integer,
+} from 'drizzle-orm/pg-core';
 
 export const linksTable = pgTable(
-  "links",
+  'links',
   {
-    id: serial("id").primaryKey().notNull(),
-    url: text("url").notNull(),
-    short: varchar("short", { length: 50 }),
-    createdAt: timestamp("created_at").defaultNow(),
+    id: serial('id').primaryKey().notNull(),
+    url: text('url').notNull(),
+    short: varchar('short', { length: 50 }),
+    createdAt: timestamp('created_at').defaultNow(),
   },
-  (links) => [uniqueIndex("url_idx").on(links.url)]
+  (links) => [uniqueIndex('url_idx').on(links.url)]
 );
+
+// links --> link --> has as many visits as it has been visited
+export const linksTableRelations = relations(linksTable, ({ many }) => ({
+  visits: many(visitsTable),
+}));
+
+export const visitsTable = pgTable('visits', {
+  id: serial('id').primaryKey().notNull(),
+  linkId: integer('link_id')
+    .notNull()
+    .references(() => linksTable.id),
+  createdAt: timestamp('created_at').defaultNow(),
+});
+
+// visits --> visit --> one link
+export const visitsTableRelations = relations(visitsTable, ({ one }) => ({
+  link: one(linksTable, {
+    fields: [visitsTable.linkId],
+    references: [linksTable.id],
+  }),
+}));
